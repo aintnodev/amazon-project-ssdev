f74d98bcd424421328de91aca71718a0e566624a
Ishaan Gupta <ishaan.ishu@proton.me>
2024-07-22 17:21:59 +0530

    Added class for product objects

--- scripts/data/products.js
+++ scripts/data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from "../utils/money.js";
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -8,6 +10,30 @@ export function getProduct(productId) {
   });
 
   return matchingProduct;
+}
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
 }
 
 export const products = [
@@ -481,4 +507,8 @@ export const products = [
     priceCents: 2400,
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
+
+console.log(products);
