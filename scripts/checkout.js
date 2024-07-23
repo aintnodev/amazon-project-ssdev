f59bd5544f381826a69395e37f33076d7e476518
Ishaan Gupta <ishaan.ishu@proton.me>
2024-07-23 15:38:09 +0530

    Use promises instead of callbacks

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,28 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../scripts/data/products.js";
+import { loadCart } from "../scripts/data/cart.js";
 
 // import "../scripts/data/cart-class.js";
 // import "../scripts/data/cart-oop.js";
-import { loadProducts } from "../scripts/data/products.js";
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+// loadProducts(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
