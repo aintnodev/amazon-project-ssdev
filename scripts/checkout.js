16b77926a23c7e85fcca6212bcd852a9a6decbf6
Ishaan Gupta <ishaan.ishu@proton.me>
2024-07-23 15:55:24 +0530

    Removed unnecessary code

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,28 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../scripts/data/products.js";
-import { loadCart } from "../scripts/data/cart.js";
+import { loadProductsFetch } from "../scripts/data/products.js";
 
+// import "../scripts/data/cart.js";
 // import "../scripts/data/cart-class.js";
 // import "../scripts/data/cart-oop.js";
 
-Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve();
-    });
-  }),
-  new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-    });
-  }),
-]).then(() => {
+loadProductsFetch().then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
-
-// loadProducts(() => {
-//   renderOrderSummary();
-//   renderPaymentSummary();
-// });
