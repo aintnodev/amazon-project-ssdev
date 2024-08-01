8bf3075b9096fd67468583dcf053a88f91ea3c5b
Ishaan Gupta <ishaan.ishu@proton.me>
2024-08-01 17:13:55 +0530

    Add support for oder tracking page

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../data/cart.js";
 import { getDeliveryOption } from "../data/deliveryOptions.js";
 import { getProduct } from "../data/products.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../data/orders.js";
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -53,9 +54,30 @@ export function renderPaymentSummary() {
       <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-place-order-button">
       Place your order
     </button>`;
 
   document.querySelector(".payment-summary").innerHTML = paymentSummaryHTML;
+
+  document
+    .querySelector(".js-place-order-button")
+    .addEventListener("click", async () => {
+      try {
+        const response = await fetch("https://supersimplebackend.dev/orders", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ cart: cart }),
+        });
+
+        const order = await response.json();
+        addOrder(order);
+      } catch (error) {
+        console.error(error);
+      }
+
+      window.location.href = "orders.html";
+    });
 }
